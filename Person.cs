c4d8a5c8d090d923178fab0b5be9515952865097
paufer2337 <paufer2337@gmail.com>
2026-05-06 23:07:17 +0200

    Initial commit with basic setup for Program.cs & Person.c

---
+++ Person.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class Person
+{    
+    private string firstName;
+    private string lastName;
+    private int age;
+
+    public Person(string firstName, string lastName, int age)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public string FirstName
+    {
+        get 
+        { 
+            return this.firstName; 
+        }
+
+        set 
+        { 
+            this.firstName = value;
+        }
+    }
+
+    public string LastName
+    {
+        get 
+        { 
+            return this.lastName; 
+        }
+
+        set 
+        { 
+            this.lastName = value; 
+        }
+    }
+
+    public int Age
+    {
+        get 
+        { 
+            return this.age; 
+        }
+        
+        set 
+        { 
+            this.age = value; 
+            }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+    }
+
+}
